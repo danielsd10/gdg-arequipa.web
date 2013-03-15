19a0448d54e5c5ad1b42b8cac9493028b65c90a0
Daniel Salas <danielsd10@gmail.com>
2013-03-15 01:17:51 -0500

    ajuste de contenidos y estilos según plantilla, corrección de slider - #header: marcador de sección activa - #programa: tabla - #speakers: imagen y reseña - #sponsors: imágenes - #colaboradores: imágenes y reseña - corrección de js de slider para el soporte de secciones con diferente alto

--- projects/site-androidday/js/main.js
+++ projects/site-androidday/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(initPage);
 
 var currentSection = '';
 var timeOutFitScreen;
+var positions = null;
 
 function initPage()
 {
@@ -31,22 +32,31 @@ function scrollPage(_target)
 	if(_target == '#tour')	$('.menu-logo').removeClass('compact');
 	else 					$('.menu-logo').addClass('compact');
 
+	if (positions===null)
+	{
+		positions = [];
+		positions[0] = ($("#tour").offset()).top - $('#header').height();
+		positions[1] = ($("#programa").offset()).top - $('#header').height();
+		positions[2] = ($("#speakers").offset()).top - $('#header').height();
+		positions[3] = ($("#sponsors").offset()).top - $('#header').height();
+		positions[4] = ($("#colaboradores").offset()).top - $('#header').height();
+	}
 	switch (currentSection)
 	{
 		case '#tour':
-			posY = altoScreen * 0;
+			posY = positions[0]; //altoScreen * 0;
 			break;
 		case '#programa':
-			posY = altoScreen * 1;
+			posY = positions[1]; //altoScreen * 1;
 			break;
 		case '#speakers':
-			posY = altoScreen * 2;
+			posY = positions[2] //altoScreen * 2;
 			break;
 		case '#sponsors':
-			posY = altoScreen * 3;
+			posY = positions[3]; //altoScreen * 3;
 			break;
 		case '#colaboradores':
-			posY = altoScreen * 4;
+			posY = positions[4]; //altoScreen * 4;
 			break;
 	}
 
@@ -59,10 +69,11 @@ function fitToScreen()
 		$('body').addClass('mobile');
 	else
 		$('body').removeClass('mobile');
-
-	var altoScreen = window.innerHeight - $('#header').height() - $("#footer").height();
-	$('#content, .visor').css('height', altoScreen + 'px');
-
+	
+	var altoScreen = window.innerHeight - $('#header').height();
+	$('#content').css('height', altoScreen + 'px');
+	$('.visor').css('min-height', altoScreen + 'px');
+	
 	if(currentSection != '')
 	{
 		clearTimeout(timeOutFitScreen);
